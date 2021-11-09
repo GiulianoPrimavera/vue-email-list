7847dca13b4d4685ce7349868398c52be0938187
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-09 16:49:11 +0100

    adds a mounted function

--- js/script.js
+++ js/script.js
@@ -7,16 +7,29 @@ const vueApp = new Vue({
         indirizziEmailRandom: [
 
         ]
-    },  
+    },
     methods: {
+        //clickando sul bottone si esegue questa funzione
         creaIndirizziEmailRandom: function(){
+            //svuoto l'array di indirizzi random
+            this.indirizziEmailRandom = []
+            //popolo l'array
             for(let i = 0; i < 10; i++){
                 axios.get(this.url).then((ajaxResponse) =>{
                     //con la funzione then() riesco a recuperare i dati che l'api vuole mostrare chiamando al suo interno .data
                     this.indirizziEmailRandom.push(ajaxResponse.data.response)
                 })
             }
-            console.log(this.indirizziEmailRandom);
+        }
+    },
+    //il browser esegue questa funzione senza alcun input da parte dell'utente, quadno l'istanza è stata "montata"
+    mounted() {
+        //popolo l'array in automatico inserendo 10 indirizzi email random
+        for (let i = 0; i < 10; i++) {
+            axios.get(this.url).then((ajaxResponse) => {
+                //con la funzione then() riesco a recuperare i dati che l'api vuole mostrare chiamando al suo interno .data
+                this.indirizziEmailRandom.push(ajaxResponse.data.response)
+            })
         }
     }
 })
