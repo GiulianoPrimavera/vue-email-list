fb000ac6cb7245047b6686693ab6f3587adbabeb
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-09 16:09:43 +0100

    adds an array and a method that fill the array with the random e-mail address

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ Vue.config.devtools = true;
 const vueApp = new Vue({
     el: "#app",
     data: {
+        url: "https://flynn.boolean.careers/exercises/api/random/mail",
+        indirizziEmailRandom: [
 
-    },
+        ]
+    },  
     methods: {
-        
+        creaIndirizziEmailRandom: function(){
+            for(let i = 0; i < 10; i++){
+                axios.get(this.url).then((ajaxResponse) =>{
+                    //con la funzione then() riesco a recuperare i dati che l'api vuole mostrare chiamando al suo interno .data
+                    this.indirizziEmailRandom.push(ajaxResponse.data.response)
+                })
+            }
+            console.log(this.indirizziEmailRandom);
+        }
     }
 })
